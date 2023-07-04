00969f38dc58c08e263db6497a01a368f779229a
MariaRipley <roblesb.maria@gmail.com>
2023-07-04 23:28:41 +0200

    Add preview card styles for mobile version

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,5 +1,5 @@
 import cover from "../../images/vibes-logo.jpg";
-import user from "../../images/user.jpeg";
+import user from "../../images/urkel.jpg";
 import "../../styles/App.scss";
 
 const Preview = ({ data }) => {
@@ -9,22 +9,24 @@ const Preview = ({ data }) => {
 
       <section className="autor">
         <section className="info-project">
-          <p className="subtitle">{data.repo || "Repo"}</p>
+          <p className="subtitle">{data.repo || "Repositorio"}</p>
           <hr className="line" />
 
-          <h2 className="title">{data.name || "Nombre del proyecto"}</h2>
-          <p className="slogan">{data.slogan || "Slogan"}</p>
+          <h2 className="title">{data.name || "Todo sobre Laura Winslow"}</h2>
+          <p className="slogan">{data.slogan || "¿He sido yo?"}</p>
           <p className="desc">{data.desc}</p>
           <section className="technologies">
-            <p className="text">{data.technologies || "Tecnologias"}</p>
-            <p className="text">{data.demo || "Demo"}</p>
+            <p className="text">
+              {data.technologies || "56k de cobre vía módem"}
+            </p>
+            <p className="text">{data.demo || "Visual Basic"}</p>
           </section>
         </section>
 
         <section className="info-autor">
           <img className="image" src={data.photo || user} alt="" />
-          <p className="job">{data.job || "Trabajo"}</p>
-          <p className="name">{data.autor || "Nombre"}</p>
+          <p className="job">{data.job || "Inventor"}</p>
+          <p className="name">{data.autor || "Steve Urkel"}</p>
         </section>
       </section>
     </section>
