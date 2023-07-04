b3dfdc34d5e5203381ae0bc0e4f1cd010811c96e
MariaPeCa <mpenacantero@gmail.com>
2023-07-04 21:22:28 +0200

    update slogan

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,11 +4,12 @@ import "../../styles/home.scss";
 function Home() {
   return (
     <div className="home">
-      <h1 className="home__title">¿Personaje de los 90s caído en el olvido?</h1>
+      <h1 className="home__title"> The 90's developers </h1>
+      <h1 className="home__title">¿Te apetece conocer nuestros proyectos?</h1>
       <p className="home__text">¡Estás en la web correcta!</p>
       <button className="home__button">
         <Link className="home__button--link" to="/main">
-          DABUTI COLEGA
+          Bienvenidxs
         </Link>
       </button>
     </div>
