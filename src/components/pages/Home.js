183387a1733a322114667f0b2c8705dc8fa1da7a
MariaRipley <roblesb.maria@gmail.com>
2023-07-04 23:11:42 +0200

    Add home styles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import "../../styles/home.scss";
 function Home() {
   return (
     <div className="home">
-      <h1 className="home__title"> The 90's developers </h1>
+      <h1 className="home__title team-name"> The 90's developers </h1>
       <h1 className="home__title">¿Te apetece conocer nuestros proyectos?</h1>
       <p className="home__text">¡Estás en la web correcta!</p>
       <button className="home__button">
