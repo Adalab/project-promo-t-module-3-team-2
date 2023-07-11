8183b219d79c7b21bc71732c00f861102b1da70c
MiriamPaternain <miriampaternain@gmail.com>
2023-07-11 12:28:39 +0200

    changes in styles of landing page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,15 @@
-import { Link } from "react-router-dom";
-import "../../styles/home.scss";
+import { Link } from 'react-router-dom';
+import '../../styles/home.scss';
 
 function Home() {
   return (
-    <div className="home">
-      <h1 className="home__title team-name"> The 90's developers </h1>
-      <h1 className="home__title">¿Te apetece conocer nuestros proyectos?</h1>
-      <p className="home__text">¡Estás en la web correcta!</p>
-      <button className="home__button">
-        <Link className="home__button--link" to="/main">
-          Bienvenidxs
-        </Link>
-      </button>
+    <div className='home'>
+      <h1 className='home__title team-name'> The 90's developers </h1>
+      <h1 className='home__title'>¿Te apetece conocer nuestros proyectos?</h1>
+      <p className='home__text'>¡Estás en la web correcta!</p>
+      <Link className='home__button--link' to='/main'>
+        <button className='home__button'>Bienvenidxs</button>
+      </Link>
     </div>
   );
 }
